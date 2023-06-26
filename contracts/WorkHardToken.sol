a4b25bb6274e33c672c6f9b188cd398242b77015
enpitsulin <enpitsulin@gmail.com>
2023-06-26 10:48:57 +0800

    fix: for decimals and total supply

--- contracts/WorkHardToken.sol
+++ contracts/WorkHardToken.sol
@@ -5,8 +5,12 @@ pragma solidity ^0.8.9;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WorkHardToken is ERC20 {
-    // Define the supply of FunToken: 996,996,996 
-    uint256 constant initialSupply = 996996996;
+    // Define the supply of FunToken: 996,996,996
+    uint256 constant initialSupply = 996996996 * (10 ** 5);
+
+    function decimals() public view virtual override returns (uint8) {
+        return 5;
+    }
 
     // Constructor will be called on contract creation
     constructor() ERC20("Work hard token", "WHT") {
